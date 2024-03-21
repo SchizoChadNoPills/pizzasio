9cec2f1e390a9205c474b490e3495563f9b44f29
SchizoChadNoPills <133130511+SchizoChadNoPills@users.noreply.github.com>
2024-03-21 16:46:49 +0100

    Deploy (#7)
    * feat: env
    
    * feat: env
    
    * feat: env
    
    * feat: env

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -99,27 +99,29 @@ abstract class BaseController extends Controller
     /**
      * Constructor.
      */
-    public function initController(RequestInterface $request, ResponseInterface $response,
-                                   LoggerInterface $logger)
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
-// Do Not Edit This Line
+        // Do Not Edit This Line
         parent::initController($request, $response, $logger);
         $this->session = \Config\Services::session();
-        $this->router = service('router');
+        $this->router  = service('router');
         if (! $request->isCLI()) {
             if ($this->acl && ! ($this->router->methodName() === $this->acl_dmz)) {
                 /** @var User $user */
                 $user = session()->get('user');
                 if ($user === null) {
                     $this->redirect('/Login');
+
                     exit();
                 }
                 if (! $user->getActive()) {
                     $this->session = null;
                     $this->redirect('/Login');
+
                     exit();
                 }
             }
+
             if (session()->has('messages')) {
                 $this->messages = session()->get('messages');
                 session()->remove('messages');
@@ -136,11 +138,12 @@ abstract class BaseController extends Controller
     public function redirect()
     {
         $url = implode('/', func_get_args());
-        $URL = base_url($url); // Utiliser base_url pour inclure le sous-dossier
+        $url = base_url($url); // Utiliser base_url pour inclure le sous-dossier
         header("Location: {$url}");
         if (count($this->messages) > 0) {
             session()->set('messages', $this->messages);
         }
+
         exit; /** @phpstan-ignore-line */
     }
 
